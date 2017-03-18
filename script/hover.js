8ce381bad7d82a61cd66253e73a6178a2eb70cb9
Schevling <jskjev200@cable.comcast.com>
2017-03-18 09:56:43 -0400

    Continue initial build.

--- script/hover.js
+++ script/hover.js
@@ -1,18 +1,110 @@
-/* This is the method to manage hover text elements on the page. Any link with associated
- * hover text should recieve a data attribute in the following format: data-hover-content="hover text"
- * where hover text is the content of the associated text block.  */
+(function(){
+    'use strict';
 
-var triggerHover = function triggerHover(e) {
-    var tempTarget = e.currentTarget,
-        relativePosition = tempTarget.getBoundingClientRect(),
-        w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-        h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-        curScroll = document.documentElement.scrollTop || document.body.scrollTop,
-        hovText = document.createTextNode(tempTarget.dataset.hover_content),
-        hovEl = document.createElement('div');
+    var hover = {
 
-    //determine quadrant that target exists in, this will determine arrow orientation
-    
-    //apply arrow to proper side of hover block
-    //
-};
+        currentHover: {},
+
+        isHovering: false,
+
+        hoverElement: {},
+
+        polyfills: function polyfills() {
+            // matches
+            if (!Element.prototype.matches) {
+                Element.prototype.matches =
+                    Element.prototype.matchesSelector ||
+                    Element.prototype.mozMatchesSelector ||
+                    Element.prototype.msMatchesSelector ||
+                    Element.prototype.oMatchesSelector ||
+                    Element.prototype.webkitMatchesSelector ||
+                    function (s) {
+                        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                            i = matches.length;
+
+                        while (--i >= 0 && matches.item(i) !== this) {}
+                        return i > -1;
+                    };
+            }
+        },
+
+        parseHoverTarget: function parseHoverTarget(e) {
+            for (var target=e.target; target && target!=this; target=target.parentNode) {
+                // loop parent nodes from the target to the delegation node
+                try {
+                    if (target.matches('.hover-element')) {
+                        this.isHovering = true;
+                        this.triggerHover.apply(this, [target, e]);
+                        break;
+                    }
+                }
+                catch (e) {
+                    //suppress errors
+                }
+            }
+        },
+
+        parseClearTarget: function parseClearTarget(e) {
+            for (var target=e.target; target && target!=this; target=target.parentNode) {
+                // loop parent nodes from the target to the delegation node
+                try {
+                    if (target.matches('.hover-element')) {
+                        this.isHovering = false;
+                        this.clearHover.apply(this, [target, e]);
+                        break;
+                    }
+                }
+                catch (e) {
+                    //suppress errors
+                }
+            }
+        },
+
+        bindListeners: function bindListeners() {
+            document.addEventListener('mouseover', this.parseHoverTarget.bind(this), false);
+            document.addEventListener('mouseout', this.parseClearTarget.bind(this), false);
+        },
+
+        fadeHover: function fadeHover(){
+            this.hoverElement.style.opacity = '1';
+        },
+
+        mountHover: function mountHover(target){
+            if (this.isHovering) {
+                console.log(target);
+                console.log(this.hoverElement);
+                document.querySelector('body').appendChild(this.hoverElement);
+                this.hoverElement.style.display = 'block';
+                setTimeout(this.fadeHover.bind(this), 100);
+            }
+        },
+
+        triggerHover: function triggerHover(target, e) {
+            console.log(this.isHovering);
+            var relativePosition = target.getBoundingClientRect(),
+                w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+                h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+                hoverText = target.getAttribute('data-hover-content');
+
+            this.hoverElement = document.createElement('div');
+            this.hoverElement.innerHTML = hoverText;
+            this.hoverElement.classList.add('hover-block');
+
+            setTimeout(this.mountHover.bind(this, target), 2000);
+        },
+
+        clearHover: function clearHover (target, e) {
+            document.querySelector('body').removeChild(this.hoverElement);
+        },
+
+        initialize: function initialize() {
+            this.polyfills();
+            this.bindListeners();
+        },
+
+
+    };
+
+    hover.initialize();
+
+}());
